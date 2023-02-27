acccd49f7a6471f52e9ee6d8bb3d51a85ee56020
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-27 12:41:57 -0400

    Username and Password only

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,60 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/userDB");
 
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, doc) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(doc) {
+                if(doc.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
 
 
 
