56180c4c5b497dde953aa363405bc20c53fe8d39
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-03-01 13:20:32 -0400

    OAuth2.0 sing in implemented with facebbok

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session')
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 
@@ -34,7 +35,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -72,6 +74,18 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+    });
+}
+));
+
 app.get("/", function(req, res) {
     res.render("home");
 });
@@ -82,6 +96,15 @@ app.get("/auth/google",
 
 app.get("/auth/google/secrets", 
     passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+});
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get("/auth/facebook/secrets", 
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
     function(req, res) {
     // Successful authentication, redirect secrets.
     res.redirect("/secrets");
